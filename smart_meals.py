c5dc19cc47f88ed3c515fc963c150a4df99b59ea
Yang Li <fivecentsly@gmail.com>
2020-01-24 14:59:28 -0500

    Modify app

--- smart_meals.py
+++ smart_meals.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
